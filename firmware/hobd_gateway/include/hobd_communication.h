476140ff9cf42793fa7bd77d0c196939f5809b8e
JLamb <jlamb@harbrick.com>
2016-05-26 13:43:11 -0700

    stubbed out HOBD communications interface.

---
+++ firmware/hobd_gateway/include/hobd_communication.h
@@ -0,0 +1,36 @@
+/**
+ * @file hobd_communication.h
+ * @brief TODO.
+ *
+ */
+
+
+
+
+#ifndef HOBD_COMMUNICATION_H
+#define	HOBD_COMMUNICATION_H
+
+
+
+
+#include <stdint.h>
+#include "hobd.h"
+
+
+
+
+//
+uint8_t hobdcomm_init( void );
+
+
+//
+uint8_t hobdcomm_send( void );
+
+
+//
+uint8_t hobdcomm_recv( void );
+
+
+
+
+#endif	/* HOBD_COMMUNICATION_H */
--- firmware/hobd_gateway/src/hobd_communication.c
+++ firmware/hobd_gateway/src/hobd_communication.c
@@ -1,6 +1,6 @@
 /**
- * @file main.c
- * @brief Main.
+ * @file hobd_communication.c
+ * @brief TODO.
  *
  */
 
@@ -10,27 +10,9 @@
 #include <stdlib.h>
 #include <string.h>
 #include <avr/io.h>
-#include <avr/interrupt.h>
-#include <avr/wdt.h>
-#include <avr/pgmspace.h>
 
-//
-#include "board.h"
-
-// BSP utilities
-#include "timer8_drv.h"
-#include "timer16_drv.h"
-#include "uart_drv.h"
 #include "uart_lib.h"
-#include "rtc_drv.h"
-#include "can_drv.h"
-#include "can_lib.h"
-
-//
 #include "hobd.h"
-#include "hobd_can.h"
-#include "diagnostic_table.h"
-#include "can_communication.h"
 #include "hobd_communication.h"
 
 
@@ -58,28 +40,5 @@
 
 
 // *****************************************************
-// main
+// public definitions
 // *****************************************************
-int main( void )
-{
-    led_init();
-    led_off();
-
-    sw_init();
-    sw_enable_pullup();
-
-    while( 1 )
-    {
-        if( sw_get_state() == ON )
-        {
-            led_on();
-            delay_ms( 10 );
-        }
-        else
-        {
-            led_off();
-        }
-    }
-
-   return 0;
-}
