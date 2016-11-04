dc3aa02ae1d6aa031f82f8e80e1a360cc70d9bb5
JLamb <jlamb@harbrick.com>
2016-11-04 09:10:30 -0700

    added CAN bus open/close/read logic.

--- tools/signal_viewer/include/can.h
+++ tools/signal_viewer/include/can.h
@@ -13,6 +13,58 @@
 
 
 
+#include "time_domain.h"
+
+
+
+
+//
+typedef int can_handle;
+
+
+
+
+//
+typedef struct
+{
+    //
+    //
+    unsigned long id;
+    //
+    //
+    unsigned long dlc;
+    //
+    //
+    timestamp_ms native_rx_timestamp;
+    //
+    //
+    timestamp_ms rx_timestamp;
+    //
+    //
+    timestamp_ms rx_timestamp_mono;
+    //
+    //
+    unsigned char data[8];
+} can_frame_s;
+
+
+
+
+//
+int can_open(
+        const unsigned long system_id );
+
+
+//
+void can_close(
+        const can_handle handle );
+
+
+//
+int can_read(
+        const can_handle handle,
+        const timestamp_ms timeout,
+        can_frame_s * const frame );
 
 
 
--- tools/signal_viewer/src/can.c
+++ tools/signal_viewer/src/can.c
@@ -12,6 +12,7 @@
 #include <string.h>
 #include <unistd.h>
 
+#include "canlib.h"
 #include "time_domain.h"
 #include "can.h"
 
@@ -49,3 +50,127 @@
 // *****************************************************
 // public definitions
 // *****************************************************
+
+//
+int can_open(
+        const unsigned long system_id )
+{
+    canHandle handle = canOpenChannel( system_id, 0x40 );
+
+    if( handle < 0 )
+    {
+        printf( "failed to open CAN channel at index %lu\n", system_id );
+        handle = -1;
+    }
+
+    if( handle >= 0 )
+    {
+        const canStatus can_stat1 = canSetBusParams(
+                handle,
+                BAUD_1M,
+                0,
+                0,
+                0,
+                0,
+                0 );
+        if( can_stat1 != canOK )
+        {
+            printf( "failed to set CAN bus parameters\n" );
+            (void) canClose( handle );
+            handle = -1;
+        }
+    }
+
+    if( handle >= 0 )
+    {
+        const canStatus can_stat2 = canSetBusOutputControl(
+                handle,
+                canDRIVER_NORMAL );
+        if( can_stat2 != canOK )
+        {
+            printf( "failed to set CAN bus output control\n" );
+            (void) canClose( handle );
+            handle = -1;
+        }
+    }
+
+    if( handle >= 0 )
+    {
+        const canStatus can_stat3 = canBusOn( handle );
+        if( can_stat3 != canOK )
+        {
+            printf( "failed to set CAN bus on-state\n" );
+            (void) canClose( handle );
+            handle = -1;
+        }
+    }
+
+    return handle;
+}
+
+
+//
+void can_close(
+        const can_handle handle )
+{
+    if( handle >= 0 )
+    {
+        (void) canClose( (canHandle) handle );
+    }
+}
+
+
+//
+int can_read(
+        const can_handle handle,
+        const timestamp_ms timeout,
+        can_frame_s * const frame )
+{
+    int ret = 1;
+
+    if( handle >= 0 )
+    {
+        canStatus can_stat = canOK;
+        long msg_id = 0;
+        unsigned int msg_dlc = 0;
+        unsigned int msg_flag = 0;
+        unsigned long tstamp = 0;
+
+        if( timeout == 0 )
+        {
+            // blocking
+            can_stat = canRead(
+                    (canHandle) handle,
+                    &msg_id,
+                    (void*) frame->data,
+                    &msg_dlc,
+                    &msg_flag,
+                    &tstamp );
+        }
+        else
+        {
+            // read with timeout
+            can_stat = canReadWait(
+                    (canHandle) handle,
+                    &msg_id,
+                    (void*) frame->data,
+                    &msg_dlc,
+                    &msg_flag,
+                    &tstamp,
+                    timeout );
+        }
+
+        if( can_stat == canOK )
+        {
+            frame->rx_timestamp = time_get_timestamp();
+            frame->rx_timestamp_mono = time_get_monotonic_timestamp();
+            frame->native_rx_timestamp = (timestamp_ms) tstamp;
+            frame->id = (unsigned long) msg_id;
+            frame->dlc  = (unsigned long) msg_dlc;
+
+            ret = 0;
+        }
+    }
+
+    return ret;
+}
